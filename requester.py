fc1d2c0cf4b435686b5d3b3e9c60de228cdfd3cf
Asus <shuhratabduraxmonov08@gmail.com>
2025-02-17 19:22:05 +0500

    requester changes

--- requester.py
+++ requester.py
@@ -11,24 +11,28 @@ GROUP_ID = '-1002438140253'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-tour_number = 716
+tour_number = 717
 
 sign_in_url = "https://api.zakovatklubi.uz/v1/user/sign-in?&_l=uz"
 reg_url = "https://api.zakovatklubi.uz/v1/user/request?&_l=uz"
-url = f"https://api.zakovatklubi.uz/v1/tournament/{tour_number}?include=clubCount,personCount,teamCount,categories,file,seasons,tours.matches,is_subscription&_l=uz"
-headers = {
-    "accept": "application/json",
-    "authorization": "Bearer Bvy8EtiU3eBRzOuUQ_sDx4FhsyIKlZPdqHAWsJBaYF88QqXALXN4Ro5aQY8pTLpm"
-}
+tour_url = f"https://api.zakovatklubi.uz/v1/tournament/{tour_number}?include=clubCount,personCount,teamCount,categories,file,seasons,tours.matches,is_subscription&_l=uz"
+transfers_url = "https://api.zakovatklubi.uz/v1/user/transfer-request?include=team,club,person,tournament,coordinator,oldClub&per-page=5&sort=-created_at&_l=uz"
 
-sign_params = [{
+
+sign_params = [
+    {
     "phone": "+998994246558",
     "password": "Sirojiddin2004;"
-},
+    },
     {
-        "phone": "+998935011112",
-        "password": "Shoaziz17"
-    }]
+       "phone": "+998500060244",
+        "password": "90M2EZUV"
+    },
+    {
+        "phone": "+998934246558",
+        "password": "Sirojiddin2004;"
+    }
+]
 
 
 def sign_in(sign_param):
@@ -40,12 +44,16 @@ def sign_in(sign_param):
 
 
 reg_headers1 = {
-    "accept": "application/json",
-    "authorization": f"Bearer {sign_in(sign_params[0])}"
+    "Accept": "application/json",
+    "Authorization": f"Bearer {sign_in(sign_params[0])}"
 }
 reg_headers2 = {
-    "accept": "application/json",
-    "authorization": f"Bearer {sign_in(sign_params[1])} "
+    "Accept": "application/json",
+    "Authorization": f"Bearer {sign_in(sign_params[1])}"
+}
+reg_headers3 = {
+    "Accept": "application/json",
+    "Authorization": f"Bearer {sign_in(sign_params[2])}"
 }
 
 params1 = {
@@ -60,52 +68,51 @@ params1 = {
     "reservePersonIds": []
 }
 
-params2 = {
-    "title": '"4ever young"',
-    "description": "null",
-    "logo_id": 234401,
-    "club_id": 10035,
-    "team_id": 27082,
-    "type": 1,
-    "tournament_id": tour_number,
-    "mainPersonIds": [165782, 332878, 332879, 332881, 364919],
-    "reservePersonIds": []
-}
-
 
 def fetch_data_from_api():
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200 and "Quiz" in response.json()['data']['title']:
+    response = requests.get(tour_url)
+    if response.status_code == 200:
         data = response.json()
         quiz_title = data['data']['title']
         text = f"""{quiz_title} quizga ro'yxatdan o'tish boshlandi
-"""
-
+url: https://zakovatklubi.uz/tournaments/{tour_number}"""
         return text
     else:
         return None
 
 
 def auto_register():
-    response1 = requests.post(reg_url, headers=reg_headers1, params=params1)
-    response2 = requests.post(reg_url, headers=reg_headers2, params=params2)
-    print(response1.json())
-    if response1.json()['message'] == "So'rov muvaffaqiyatli yuborildi" and response2.json()[
-        'message'] == "So'rov muvaffaqiyatli yuborildi":
-        return "Jamoalar muvaffaqiyatli ro'yxatdan o'tkazildi!"
+    response1 = requests.post(reg_url, headers=reg_headers1, json=params1)
+    if response1.json()['message'] == "So'rov muvaffaqiyatli yuborildi":
+        return "null muvaffaqiyatli ro'yxatdan o'tkazildi!"
     else:
         return None
 
+def accept_transfer():
+    id_response = requests.get(transfers_url, headers=reg_headers2)
+    id_response1 = requests.get(transfers_url, headers=reg_headers3)
+    if id_response.json()['data'][0]['tournament_id'] == tour_number:
+        transfer_id1 = id_response.json()['data'][0]['id']
+        transfer_id2 = id_response1.json()['data'][0]['id']
+        transfer_accept_url1 = f"https://api.zakovatklubi.uz/v1/user/accept-transfer-request/{transfer_id1}?&_l=uz"
+        transfer_accept_url2 = f"https://api.zakovatklubi.uz/v1/user/accept-transfer-request/{transfer_id2}?&_l=uz"
+        response = requests.post(transfer_accept_url1, headers=reg_headers2)
+        response1 = requests.post(transfer_accept_url2, headers=reg_headers3)
+        if response.json()['code'] == 1 and response1.json()['code'] == 1:
+            return "Transferlar qabul qilindi!"
+        else:
+            return None
+
 
 async def send_data_to_group():
-    # Fetch data from the API
     data = fetch_data_from_api()
 
     if data:
-        message = await bot.send_message(GROUP_ID, data)
-        await bot.pin_chat_message(GROUP_ID, message.message_id)
+        await bot.send_message(GROUP_ID, data)
         reg_text = auto_register()
         await bot.send_message(GROUP_ID, reg_text)
+        accept_text = accept_transfer()
+        await bot.send_message(GROUP_ID, accept_text)
         os.system('pkill -f requester.py')
     else:
         print("No data received from API.")
